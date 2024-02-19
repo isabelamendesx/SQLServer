c48e776219454bcf662fc861aa2134d0da2c0a67
isabelamendesx <gomesisabela13@gmail.com>
2024-02-19 10:39:28 -0300

    fazendo um SELECT com SqlClient

--- ConnectionString/Program.cs
+++ ConnectionString/Program.cs
@@ -6,8 +6,7 @@ namespace ConnectionString
     {
         static void Main(string[] args)
         {
-            const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
-
+            const string ConnectionString = "Server = localhost,1433; Database = balta; User ID = sa; Password = 1q2w3e4r@#$; TrustServerCertificate=True";
             // Package = Microsoft.Data.SqlClient
             // var connection = new SqlConnection();
             // connection.Open();
@@ -16,9 +15,26 @@ namespace ConnectionString
 
             // Otimizando com o using (garante que a conexão vai ser fechada quando terminar)
 
-            using(var connection = new SqlConnection(ConnectionString))
+            using (var connection = new SqlConnection(ConnectionString))
             {
+                connection.Open();
 
+                // Querys sempre dentro do using
+                using(var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    // SqlDataReader - Objeto mais fácil e rápido para ler do bd
+                    // ExecuteNonQuery - Para operações que não são de leitura
+                    var reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader.GetGuid(0)} | Title {reader.GetString(1)}");
+                    }
+                }
             }
         }
 
