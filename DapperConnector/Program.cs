e1120831ed9f44230e4742a3851768ba8296a07c
isabelamendesx <gomesisabela13@gmail.com>
2024-02-19 10:56:58 -0300

    fazendo SELECT com Dapper

---
+++ DapperConnector/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using DapperConnector.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DapperConnector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ConnectionString = "Server = localhost,1433; Database = balta; User ID = sa; Password = 1q2w3e4r@#$; TrustServerCertificate=True";
+        
+            using(var connection = new SqlConnection(ConnectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                
+                foreach (var item in categories) 
+                    Console.WriteLine($"Id: {item.Id} | Title: {item.Title}");
+            }
+        }
+    }
+}
