d3393ef1326da03a671e0ffd492bef7a15a76b1c
isabelamendesx <gomesisabela13@gmail.com>
2024-02-19 10:19:30 -0300

    Add project files.

---
+++ ConnectionString/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ConnectionString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ConnectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            // Package = Microsoft.Data.SqlClient
+            // var connection = new SqlConnection();
+            // connection.Open();
+            // connection.Close(); // Fecha a conexão
+            // connection.Dispose(); - Destrói a conexão, preciso dar um new para uma nova
+
+            // Otimizando com o using (garante que a conexão vai ser fechada quando terminar)
+
+            using(var connection = new SqlConnection(ConnectionString))
+            {
+
+            }
+        }
+
+    }
+}
